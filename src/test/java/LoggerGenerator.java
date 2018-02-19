2c054d4cadd36cc8a40ef24be1cc5286f6e57968
hwbaker <hewei860311@sohu.com>
2018-02-19 12:36:09 +0800

    Spark Streaming整合Kafka和Flume

--- src/test/java/LoggerGenerator.java
+++ src/test/java/LoggerGenerator.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
  * 模拟日志产生
  */
 public class LoggerGenerator {
+
     private static Logger logger = Logger.getLogger(LoggerGenerator.class.getName());
 
     public static void main(String[] args) throws Exception{
