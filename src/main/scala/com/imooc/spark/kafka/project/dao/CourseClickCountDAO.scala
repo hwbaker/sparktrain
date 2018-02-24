ab78ff26469eb56afd8e48efce823bb982646ff9
hwbaker <hewei860311@sohu.com>
2018-02-24 14:15:34 +0800

    访问DAO层方法实现

--- src/main/scala/com/imooc/spark/kafka/project/dao/CourseClickCountDAO.scala
+++ src/main/scala/com/imooc/spark/kafka/project/dao/CourseClickCountDAO.scala
@@ -1,13 +1,16 @@
 package com.imooc.spark.kafka.project.dao
 
 import com.imooc.spark.kafka.project.domain.CourseClickCount
+import com.imooc.spark.project.utils.HBaseUtils
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.util.Bytes
 
 import scala.collection.mutable.ListBuffer
 
 /**
   * 实战课程点击数数据库访问层
   */
-case class CourseClickCountDAO() {
+object CourseClickCountDAO {
 
   val tableName = "imooc_course_clickcount"
   val cf = "info"
@@ -18,7 +21,15 @@ case class CourseClickCountDAO() {
     * @param list CourseClickCount集合
     */
   def save(list: ListBuffer[CourseClickCount]): Unit = {
+    val table = HBaseUtils.getInstance().getTable(tableName)
 
+    for (ele <- list) {
+      table.incrementColumnValue(Bytes.toBytes(ele.day_courseid),
+        Bytes.toBytes(cf),
+        Bytes.toBytes(qualifer),
+        ele.click_count
+      )
+    }
   }
 
   /**
@@ -27,7 +38,33 @@ case class CourseClickCountDAO() {
     * @return
     */
   def select(day_courseid: String):Long = {
-    0l
+    val table = HBaseUtils.getInstance().getTable(tableName)
+
+    val gets = new Get(Bytes.toBytes(day_courseid))
+    val value = table.get(gets).getValue(cf.getBytes, qualifer.getBytes)
+
+    if (value == null) {
+      0L
+    } else {
+      Bytes.toLong(value)
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    var showStr = select("20171111_10") + ":" + select("20171111_7") + ":" + select("20171111_8") + ":" + select("20171111_9")
+    println(showStr)
+
+    val list = new ListBuffer[CourseClickCount]
+
+
+    list.append(CourseClickCount("20171111_10", 10))
+    list.append(CourseClickCount("20171111_7", 7))
+    list.append(CourseClickCount("20171111_8", 8))
+    list.append(CourseClickCount("20171111_9", 9))
+
+    save(list)
+    println(select("20171111_10") + ":" + select("20171111_7") + ":" + select("20171111_8") + ":" + select("20171111_9"))
   }
 
 
--- src/main/scala/com/imooc/spark/kafka/project/domain/CourseClickCount.scala
+++ src/main/scala/com/imooc/spark/kafka/project/domain/CourseClickCount.scala
@@ -3,6 +3,6 @@ package com.imooc.spark.kafka.project.domain
 /**
   * 实战课程点击数实体类
   * @param day_courseid 对应的HBase中的Rowkey:20171111_1
-  * @param click        对应的20171111_1访问总数
+  * @param click_count        对应的20171111_1访问总数
   */
-case class CourseClickCount(day_courseid:String, click:Long)
+case class CourseClickCount(day_courseid:String, click_count:Long)
